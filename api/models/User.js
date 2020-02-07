9b9476d565a6c59fecc9304fdcb27ed5d264e457
Prabhat Singh <prabhat.singh@vcareall.com>
2020-02-07 11:47:12 +0530

    Updated Code - [List with Pagination, User Data By Its ID]

--- api/models/User.js
+++ api/models/User.js
@@ -1,21 +1,7 @@
 const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
 
-const userSchema = mongoose.Schema({
-    _id : mongoose.Schema.Types.ObjectId,
-    customerName : String,
-    // ADDRESS : String,
-    // ADDRESS_B : String,
-    // LOCATION : String,
-    // CITY : String,
-    // IZO_SDWAN_DEVICE_NAMES : String,
-    // IZO_SDWAN_SRVC_ID : String,
-    // CUSTOMER_SERVICE_ID : String,
-    // SERVICE_TYPE : String,
-    // CPE_INTERFACE : String,
-    // PRODUCT_FLAVOUR : String,
-    // CPE_FAILOVER : String,
-    // PACKAGE_TYPE : String,
-}) 
-
+const userSchema = mongoose.Schema() 
+userSchema.plugin(mongoosePaginate);
 
 module.exports  = mongoose.model('User', userSchema);
--- api/routes/userlist.js
+++ api/routes/userlist.js
@@ -1,31 +1,29 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const route = express.Router();
 const Users = require('../models/User');
 
-// route.post('/add', (req, res, next)=>{
-//     const newuser=new Users({
-//            _id: new mongoose.Types.ObjectId(),
-//            customerName: "Satya"
-//        });
-//        newuser.save().then(docs=>{    
-//            console.log(docs);      
-//            res.status(201).json({
-//                message:"user added successfully",
-//                customer:docs
-//            });
-//        }).catch(err=>{throw err;});
-// })
-
 route.get('/list', (req, res, next)=>{
-   
-       Users.findById('5e3c21d176abe4438fcf98ac').then(docs=>{    
-           console.log(docs);      
-           res.status(201).json({
-               message:"user fetched successfully",
-               customer:docs
-           });
-       }).catch(err=>{throw err;});
+    var page = req.param('page') || 1;
+    const options = {
+        page: page,
+        limit: 10,
+        collation: {
+          locale: 'en'
+        }
+    };
+    Users.paginate({}, options).then(docs=>{    
+        res.status(200).json(docs);
+    }).catch(err=>{throw err;});
+})
+
+route.get('/list/:id', (req, res, next)=>{
+    var id = req.params.id;
+    Users.findById(id).limit(10).then(docs=>{    
+        res.status(201).json({
+            message:"user fetched successfully",
+            customer:docs
+        });
+    }).catch(err=>{throw err;});
 })
 
 module.exports = route;
