f87412b52e0287651dfe1338296a671cdbbeb9ac
Prabhat Singh <prabhat.singh@vcareall.com>
2020-02-07 12:45:14 +0530

    no message

--- api/routes/userlist.js
+++ api/routes/userlist.js
@@ -3,23 +3,31 @@ const route = express.Router();
 const Users = require('../models/User');
 
 route.get('/list', (req, res, next)=>{
-    var page = req.param('page') || 1;
+    var page = req.params.page || 1;
     const options = {
+        select: '_id CUSTOMER_NAME',
         page: page,
         limit: 10,
-        collation: {
-          locale: 'en'
-        }
     };
+
     Users.paginate({}, options).then(docs=>{    
         res.status(200).json(docs);
     }).catch(err=>{throw err;});
 })
 
-route.get('/list/:id', (req, res, next)=>{
+route.get('/filter', (req, res, next)=>{
+    Users.find().distinct('CPE_FAILOVER').then(docs=>{    
+        res.status(200).json({
+            message:"user fetched successfully",
+            customer:docs
+        });
+    }).catch(err=>{throw err;});
+})
+
+route.get('/:id', (req, res, next)=>{
     var id = req.params.id;
     Users.findById(id).limit(10).then(docs=>{    
-        res.status(201).json({
+        res.status(200).json({
             message:"user fetched successfully",
             customer:docs
         });
