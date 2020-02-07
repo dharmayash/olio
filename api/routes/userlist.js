500134d36914a50231e819b3d6db0ab10d6b23e7
Prabhat Singh <prabhat.singh@vcareall.com>
2020-02-07 14:28:18 +0530

    Updated Final Code

--- api/routes/userlist.js
+++ api/routes/userlist.js
@@ -3,7 +3,7 @@ const route = express.Router();
 const Users = require('../models/User');
 
 route.get('/list', (req, res, next)=>{
-    var page = req.params.page || 1;
+    var page = req.param('page') || 1;
     const options = {
         select: '_id CUSTOMER_NAME',
         page: page,
@@ -15,22 +15,45 @@ route.get('/list', (req, res, next)=>{
     }).catch(err=>{throw err;});
 })
 
-route.get('/filter', (req, res, next)=>{
-    Users.find().distinct('CPE_FAILOVER').then(docs=>{    
-        res.status(200).json({
-            message:"user fetched successfully",
-            customer:docs
-        });
+route.get('/filter/by/city/:city', (req, res, next)=>{
+    var page = req.param('page') || 1;
+    var city = req.params.city;
+    if(!city) res.status(500).json({message : 'Enter City !!'});
+    const query = {
+        CITY: {$regex: city, $options: 'i'}
+    };
+    const options = {
+        select: '_id CUSTOMER_NAME',
+        page: page,
+        limit: 10,
+    };
+    Users.paginate(query, options).then(docs=>{    
+        res.status(200).json(docs);
     }).catch(err=>{throw err;});
 })
 
-route.get('/:id', (req, res, next)=>{
+route.get('/graph/by/city', (req, res, next)=>{
+    const aggregate = [
+        {
+            $group: {
+                _id: '$CITY',
+                count: { $sum: 1 }
+            }
+        }, { 
+            $sort: { count : -1 }
+        },{
+            $limit : 10
+        }
+    ];
+    Users.aggregate(aggregate).then(docs=>{    
+        res.status(200).json(docs);
+    }).catch(err=>{throw err;});
+})
+
+route.get('/detail/:id', (req, res, next)=>{
     var id = req.params.id;
     Users.findById(id).limit(10).then(docs=>{    
-        res.status(200).json({
-            message:"user fetched successfully",
-            customer:docs
-        });
+        res.status(200).json(docs);
     }).catch(err=>{throw err;});
 })
 
