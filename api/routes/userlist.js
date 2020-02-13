97b749a2e8dec1544518c4cef9037aaa80904d89
Prabhat Singh <prabhat.singh@vcareall.com>
2020-02-13 18:23:22 +0530

    Added Topology API

--- api/routes/userlist.js
+++ api/routes/userlist.js
@@ -24,7 +24,7 @@ route.get('/filter/by/city/:city', (req, res, next)=>{
     var city = req.params.city;
     if(!city) res.status(500).json({message : 'Enter City !!'});
     const query = {
-        CITY: {$regex: city, $options: 'i'}
+        CITY: { $regex: city, $options: 'i' }
     };
     const options = {
         select: selectVal,
@@ -114,6 +114,46 @@ route.get('/detail/:id', (req, res, next)=>{
     var id = req.params.id;
     Users.findById(id).limit(10).then(docs=>{    
         res.status(200).json(docs);
+    }).catch(err=>{throw err;});
+})
+
+// Network Topology
+route.get('/network/topology', (req, res, next)=>{
+    const aggregate = [
+        {
+            $group: {
+                _id: '$CUSTOMER_NAME',
+                _city : { 
+                    $addToSet : '$CITY'
+                },
+                _data : {
+                    $addToSet : { 
+                        children : {
+                            device_name : '$IZO_SDWAN_DEVICE_NAMES',
+                            service_id : '$IZO_SDWAN_SRVC_ID',
+                            customer_service_id : '$CUSTOMER_SERVICE_ID'
+                        }
+                    }
+                } 
+            }
+        }
+    ];
+    Users.aggregate(aggregate).then(docs=>{
+        var jsonDoc = [];
+        docs.forEach(d => {
+            var name = d._id;
+            var cities = d._city;
+            var child = d._data;
+            if(name && cities){
+                cities.forEach(c => {
+                    if(c){
+                        var data = {name : name, children : { [c] : child } };
+                        jsonDoc.push(data);
+                    }
+                })
+            }
+        });
+        res.status(200).json(jsonDoc);
     }).catch(err=>{throw err;});
 })
 
